b01a0c17276462bf5338d51593106c29cc23b8c5
omkar <omkar.konda@gmail.com>
2024-10-17 18:40:02 +0530

    new scheduling page added

---
+++ scheduling_V2/script.js
@@ -0,0 +1,28 @@
+window.addEventListener("DOMContentLoaded", () => {    
+    const cardsContainer = document.querySelector(".cards");    
+    const menuItems = document.querySelectorAll("#menu > li");
+
+    const animateCards = (id) => {   
+        const card = document.querySelector(`.long__card[data-card="${id}"]`);        
+        const cardYval = card.offsetTop;
+        gsap.to(cardsContainer, {
+            y: -cardYval,
+            opacity: 1,
+            duration: 0.9,                       
+            ease: "back"
+        })
+    }
+    
+    
+    menuItems.forEach(item => {           
+        item.addEventListener("click", () => {            
+            menuItems.forEach(listItem => listItem.classList.remove("active"));
+            item.classList.add("active");
+            const id = item.dataset.menuId;
+            animateCards(id);
+        })
+    })
+
+    
+
+})
